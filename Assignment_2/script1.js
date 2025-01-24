5dca6f89df85001b8949b8b97403ed751df7fb65
meetm2005 <mandanimeet2005@gmail.comclss>
2025-01-24 19:16:09 +0530

    add many changes

--- Assignment_2/script1.js
+++ Assignment_2/script1.js
@@ -1,10 +1,9 @@
-const toggleModeButton = document.getElementById("toggleMode");
-const body = document.body;
+const faqQuestions = document.querySelectorAll('.faq-question');
 
-toggleModeButton.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  const isDarkMode = body.classList.contains("dark-mode");
-  toggleModeButton.innerHTML = isDarkMode
-    ? '<i class="fas fa-sun"></i> Light Mode'
-    : '<i class="fas fa-moon"></i> Dark Mode';
-});
+        faqQuestions.forEach(question => {
+            question.addEventListener('click', () => {
+                question.classList.toggle('active');
+                const answer = question.nextElementSibling;
+                answer.classList.toggle('visible');
+            });
+        });
